084f8ea1e26432450a0c489d1caa3603670b64fa
giselabelen <gise_5291@yahoo.com.ar>
2017-04-03 17:06:01 -0300

    RESTexample server side

--- RESTexample/serverscript.py
+++ RESTexample/serverscript.py
@@ -3,45 +3,77 @@ from tornado import gen
 from tornado.ioloop import IOLoop
 import sqlite3 as sqlite
 import tornado.web
+import json
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('Hello, world')
 
-class CarHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('GET - Welcome to the CarHandler!')
+class CorrectorHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        if self.request.headers["Content-Type"].startswith("application/json"):
+            self.json_args = json.loads(self.request.body)
+        else:
+            self.json_args = None
 
     def post(self):
-        self.write('POST - Welcome to the CarHandler!')
+        print "Guardando datos"
+        nombre = self.json_args["nombre"]
+        ejercicio = self.json_args["ejercicio"]
+        respuesta = self.json_args["respuesta"]
+        _execute("INSERT INTO resultados (nombre,ejercicio,respuesta) VALUES ('{0}','{1}','{2}')".format(nombre,ejercicio,respuesta))
 
-def verifyDatabase():
-    conn = sqlite.connect('cars.db')
-    c = conn.cursor()
-    try:
-        c.execute('SELECT * FROM cars')
-        print('Table already exists')
-    except:
-        print('Creating table \'cars\'')
-        c.execute('CREATE TABLE cars (\
-            id text,\
-            make text,\
-            model text,\
-            year text,\
-            trans text,\
-            color text)')
-        print('Successfully created table \'cars\'')
-    conn.commit()
-    conn.close()
+        print "Evaluando respuesta"
+        if respuesta == "1":
+            correccion = {"aprobo":True}
+        else:
+            correccion = {"aprobo":False}
+        self.write(correccion)   
+
+        print "Listo"
+
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/?", MainHandler),
-            (r"/api/v1/cars/?", CarHandler),
-            (r"/api/v1/cars/[0-9][0-9][0-9][0-9]/?", CarHandler)
+            (r"/corrector/?", CorrectorHandler)
         ]
         tornado.web.Application.__init__(self, handlers)
+
+
+def _execute(query):
+    """Function to execute queries against a local sqlite database"""
+    dbPath = 'resultados.db'
+    connection = sqlite.connect(dbPath)
+    cursorobj = connection.cursor()
+    try:
+            cursorobj.execute(query)
+            result = cursorobj.fetchall()
+            connection.commit()
+    except Exception:
+            raise
+    connection.close()
+    return result
+
+
+def verifyDatabase():
+    # conn = sqlite.connect('resultados.db')
+    # c = conn.cursor()
+    try:
+        #c.execute('SELECT * FROM resultados')
+        _execute('SELECT * FROM resultados')
+        print('Table already exists')
+    except:
+        print('Creating table \'resultados\'')
+        # c.execute('CREATE TABLE resultados (\
+        #     nombre text,\
+        #     ejercicio text,\
+        #     respuesta text)')
+        _execute('CREATE TABLE resultados (\
+            nombre text,\
+            ejercicio text,\
+            respuesta text)')
+        print('Successfully created table \'resultados\'')
+    # conn.commit()
+    # conn.close()
+
 
 def main():
 
