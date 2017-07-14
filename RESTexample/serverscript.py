6b970050459eaced54aafc781bea39b3501b5309
giselabelen <gise_5291@yahoo.com.ar>
2017-07-14 12:25:45 -0300

    ok outcomes handler, solo falta que guarde la nota

--- RESTexample/serverscript.py
+++ RESTexample/serverscript.py
@@ -68,61 +68,69 @@ class OutcomesHandler(tornado.web.RequestHandler):
 
     def post(self):
         print ("Obteniendo datos")
-        # print (self.request.body)   
 
-        root = etree.Element(u'imsx_POXEnvelopeResponse',
-                         xmlns=u'http://www.imsglobal.org/services/'
-                               u'ltiv1p1/xsd/imsoms_v1p0')
+        # namespaces del xml input
+        ns = {'ns':'http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0'}
 
-        header = etree.SubElement(root, 'imsx_POXHeader')
-        header_info = etree.SubElement(header, 'imsx_POXResponseHeaderInfo')
-        version = etree.SubElement(header_info, 'imsx_version')
-        version.text = 'V1.0'
-        # message_identifier = etree.SubElement(header_info,
-        #                                       'imsx_messageIdentifier')
-        # message_identifier.text = message_identifier_id
-        status_info = etree.SubElement(header_info,'imsx_statusInfo')
-        code_major = etree.SubElement(status_info,'imsx_codeMajor')
-        code_major.text = 'success'
-        severity = etree.SubElement(status_info,'imsx_severity')
-        severity.text = 'status'
-        #message_ref_identifier = etree.SubElement(status_info,'imsx_messageRefIdentifier')
-        operation_ref_identifier = etree.SubElement(status_info,'imsx_operationRefIdentifier')
-        operation_ref_identifier.text = 'replaceResult'
-
-        body = etree.SubElement(root, 'imsx_POXBody')
-        replace_result_response = etree.SubElement(body, 'replaceResultResponse')
-
-        ret = "<?xml version='1.0' encoding='utf-8'?>\n{}".format(
-            etree.tostring(root, encoding='utf-8'))
-
-        print("XML Response: \n%s", ret)
-        #return ret
-
-        # tipo = self.get_argument("lti_message_type")
-        # sourcedid = self.get_argument("sourcedid")
-        # score = self.get_argument("result_resultscore_textstring")
+        # nodo raiz del xml input
+        req_tree = etree.fromstring(self.request.body)
         
-        # tipo = data[0]
-        # sourcedid = data[1]
-        # score = data[2]
+        # message id del xml input
+        req_msg_id_node = req_tree.find('ns:imsx_POXHeader/ns:imsx_POXRequestHeaderInfo/ns:imsx_messageIdentifier',ns)
+        req_msg_id = req_msg_id_node.text
 
-        # print ("tipo:"+tipo)
-        # print ("sourcedid:"+sourcedid)
-        # print ("score:"+score)
+        # nodo operacion del xml input 
+        # reviso que sea replaceResult
+        req_oper_node = req_tree.find('ns:imsx_POXBody/ns:replaceResultRequest',ns)
+        if req_oper_node is None : # si no era replaceResult
+            # veo si es read o delete
+            code_major = 'unsopported'
+            req_oper_read = req_tree.find('ns:imsx_POXBody/ns:readResultRequest',ns)
+            req_oper_delete = req_tree.find('ns:imsx_POXBody/ns:deleteResultRequest',ns)
+
+            if req_oper_read is not None: # si es read
+                req_oper = 'readResult'
+                severity = 'status'
+
+            elif req_oper_delete is not None: # si es delete
+                req_oper = 'deleteResult'
+                severity = 'status'
+
+            else: # si no es read ni delete
+                req_oper = 'unknown' # esto podria ser mas declarativo... otro dia
+                severity = 'error'
+
+        else: # si era replaceResult
+            code_major = 'success'
+            severity = 'status'
+            req_oper = 'replaceResult'
+
+            # sourcedid, donde voy a guardar la nota recibida
+            req_sourcedid_node = req_oper_node.find('ns:resultRecord/ns:sourcedGUID/ns:sourcedId',ns)
+            req_sourcedid = req_sourcedid_node.text
+
+            # nota recibida
+            req_result_node = req_oper_node.find('ns:resultRecord/ns:result/ns:resultScore/ns:textString',ns)
+            req_result = req_result_node.text
+
+            # chequeo que la nota sea numerica decimal entre 0 y 1
+            try:
+                val = float(req_result)
+                if not (0 <= val <= 1.0):
+                    code_major = 'faliure'
+                else:
+                    # hacer lo correspondiente para guardar la nota
+                    pass
+            except ValueError:
+                code_major = "faliure"
+
+        # nuestra respuesta
+        xml_response = armar_xml(code_major,severity,req_msg_id,req_oper)
 
         # print ("Guardando datos")
         # _execute("INSERT INTO resultados (nombre,ejercicio,respuesta) VALUES ('{0}','{1}','{2}')".format(nombre,ejercicio,respuesta))
 
-        # print ("Evaluando respuesta")
-        # if respuesta == "1":
-        #     correccion = {"aprobo":True}
-        # else:
-        #     correccion = {"aprobo":False}
-
-        # por cross-domain
-#        self.add_header("Access-Control-Allow-Origin","http://localhost:8888")
-        self.write(ret)   
+        self.write(xml_response)   
         self.set_header('Content-Type', 'text/xml')
 
         print ("Listo")
@@ -172,6 +180,40 @@ def verifyDatabase():
         print('Successfully created table \'resultados\'')
     # conn.commit()
     # conn.close()
+
+
+def armar_xml(code_major,severity,req_msg_id,req_oper):
+    root = etree.Element(u'imsx_POXEnvelopeResponse',
+                 xmlns=u'http://www.imsglobal.org/services/'
+                       u'ltiv1p1/xsd/imsoms_v1p0')
+
+    header = etree.SubElement(root, 'imsx_POXHeader')
+    header_info = etree.SubElement(header, 'imsx_POXResponseHeaderInfo')
+    version = etree.SubElement(header_info, 'imsx_version')
+    version.text = 'V1.0'
+    # message_identifier = etree.SubElement(header_info,
+    #                                       'imsx_messageIdentifier')
+    # message_identifier.text = message_identifier_id
+    status_info = etree.SubElement(header_info,'imsx_statusInfo')
+    code_major_node = etree.SubElement(status_info,'imsx_codeMajor')
+    code_major_node.text = code_major
+    severity_node = etree.SubElement(status_info,'imsx_severity')
+    severity_node.text = severity
+    message_ref_identifier = etree.SubElement(status_info,'imsx_messageRefIdentifier')
+    message_ref_identifier.text = req_msg_id
+    operation_ref_identifier = etree.SubElement(status_info,'imsx_operationRefIdentifier')
+    operation_ref_identifier.text = req_oper
+
+    body = etree.SubElement(root, 'imsx_POXBody')
+    replace_result_response = etree.SubElement(body, 'replaceResultResponse')
+
+    ret = "<?xml version='1.0' encoding='utf-8'?>\n{}".format(
+        etree.tostring(root, encoding='utf-8'))
+
+    # print("XML Response: \n%s", ret)
+    return ret
+
+
 
 
 def main():
