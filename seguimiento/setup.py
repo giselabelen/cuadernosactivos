e49f05f5e12214e66b31b8859b975e344e4b5115
giselabelen <gise_5291@yahoo.com.ar>
2021-08-12 16:42:08 -0300

    se añade setup de extension seguimiento

---
+++ seguimiento/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='seguimiento',
+      version='0.1',
+      description='POC secuencias de ejercicios con seguimiento de actividad',
+      #url=,
+      author='Gisela Confalonieri',
+      #author_email=,
+      #license=,
+      packages=['seguimiento'])
+      #zip_safe=)
